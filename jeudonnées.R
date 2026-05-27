cdc126ae1f334e1e9a50b21b51a6420194f5e13c
Romain Ben <romainben31@gmail.com>
2026-05-27 10:13:42 +0200

    fix: corriger les noms de variables dans la generation des donnees

--- jeudonnées.R
+++ jeudonnées.R
@@ -1,61 +1,39 @@
-## géneration du jeu de données 
 set.seed(1)
-## on va faire plusierus jeu de données pour mettre en évidences les différences 
 
-n  <- 1000    # Nombre d'observations
-li <- 0      # Limite inférieure du support
-la <- 10     # Limite supérieure du support
+n  <- 1000
+li <- 0
+la <- 10
 
 X <- runif(n, min = li, max = la)
 
-a_réel <- 5
-b_réel <- 5
-f_réel <- a_réel * X + b_réel
+a_reel <- 5
+b_reel <- 5
+signal_lineaire <- a_reel * X + b_reel
 
-# on passe a la création des bruits 
-
-# bruit centré a variance constante ( sd = 1)
-b_centré <- rnorm(n,mean=0, sd = 1)
-# bruit non centré ( mean = 1) a variance constante ( sd = 1)
-b_n_centré <- rnorm(n,mean=1, sd = 1)
-# bruit centré a variance non constante ( sd = X*2)
-b_centré_variance_n_const <- rnorm(n,mean=0, sd = X*2)
-# bruit non centré a variance non constante ( sd = X*2)
-b_n_centré_variance_n_const <- rnorm(n, mean = 1, sd = 0.5 * X)
-
-## faire le data frame 
+bruit_ideal       <- rnorm(n, mean = 0, sd = 1)
+bruit_non_centre  <- rnorm(n, mean = 1, sd = 1)
+bruit_hetero      <- rnorm(n, mean = 0, sd = X * 2)
+bruit_pire        <- rnorm(n, mean = 1, sd = 0.5 * X)
 
 df_lineaire <- data.frame(
-  X = X,
+  X            = X,
   Y_ideal      = signal_lineaire + bruit_ideal,
   Y_non_centre = signal_lineaire + bruit_non_centre,
   Y_hetero     = signal_lineaire + bruit_hetero,
   Y_pire       = signal_lineaire + bruit_pire
 )
 
-## le cas avec les trucs pas linéaire 
-
-
-c_réel <- -1
-
-# Modèle aX^2 + b
-Y_quadra_simple <- a_réel * (X^2) + b_réel + b_centré
-
-# Modèle aX^2 + bX + c
-Y_poly_complet  <- a_réel * (X^2) + b_réel * X + c_réel + b_centré
+c_reel <- -1
 
 df_polynomial <- data.frame(
-  X = X,
-  Y_quadra_simple = Y_quadra_simple,
-  Y_poly_complet  = Y_poly_complet
+  X               = X,
+  Y_quadra_simple = a_reel * X^2 + b_reel + bruit_ideal,
+  Y_poly_complet  = a_reel * X^2 + b_reel * X + c_reel + bruit_ideal
 )
 
-if(!dir.exists("data")) dir.create("data")
+if (!dir.exists("data")) dir.create("data")
 
-write.csv(df_lineaire, "data/donnees_lineaires.csv", row.names = FALSE)
+write.csv(df_lineaire,   "data/donnees_lineaires.csv",   row.names = FALSE)
 write.csv(df_polynomial, "data/donnees_polynomiales.csv", row.names = FALSE)
 
-cat("Génération finie")
-
-
-
+cat("Génération finie\n")
